b3ec748488a8c7e3b6c4f27ae61745f624ae75bb
Anonymous0perator <33887574+Anonymous0perator@users.noreply.github.com>
2018-03-13 19:02:59 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -129,12 +129,26 @@ client.on('message', message => {
 
   if (commandIs("purge")) {
     if (message.member.hasPermission("MANAGE_MESSAGES")) {
-      message.channel.fetchMessages()
-        .then(function(list) {
-          message.channel.bulkDelete(list);
-        }, function(err) {
-          message.channel.send("ERROR: ERROR CLEARING CHANNEL.")
-        })
+     try {
+      if (!Number(message.content.split(" ")[1])) {
+        message.channel.send("Please provide a number for amount of messages to be deleted.")
+        return;
+      }
+      let deleteCount = parseInt(message.content.split(" ")[1])
+      if (deleteCount > 99) {
+        message.channel.send("Yikes! The max limit for message bulkDelete is 100");
+        return;
+      }
+
+      message.channel.fetchMessages({
+        limit: deleteCount + 1
+      }).then(messages => message.channel.bulkDelete(messages));
+      message.channel.send("Purged and cleared!").then(msg => msg.delete())
+
+
+    } catch (err) {
+      message.channel.send(ess.errorHandle(err));
+    }
     }
   }
   if(commandIs("warn")){
