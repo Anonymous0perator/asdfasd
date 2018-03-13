8d18a60727dbe51de4ab59a28d81f2012004168a
Anonymous0perator <33887574+Anonymous0perator@users.noreply.github.com>
2018-03-13 19:06:29 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -113,16 +113,58 @@ client.on('message', message => {
   }
 
     if(commandIs("mute")) {
-      client.muteUser = function (user, channel, callback) {
-          var object = {"readMessages": false, "sendMessages": false};
-          self.bot.overwritePermissions(channel, user, object, callback);
+	   if(message.member.hasPermission("MANAGE_MESSAGES")){
+	   try {
+      let role = message.guild.roles.find("name", "Muted");
+      let member = message.mentions.members.first();
+      let bot = message.guild.member(client.user);
+
+      if (!member) {
+        message.channel.send("Please mention a valid member in this guild.");
+        return;
       }
+      if (!role) {
+        message.guild.createRole({
+          name: 'Muted',
+          color: 'BLACK',
+        }).catch(err => message.channel.send(ess.errorHandle(err)))
+        message.channel.send("There was no Muted role so I created it. Try the mute command again.")
+        return;
+      }
+
+      member.setMute(true)
+      message.channel.overwritePermissions(member, {
+        "SEND_MESSAGES": false
+      })
+      member.addRole(role)
+      message.channel.send("Muted!")
+    } catch (err) {
+      message.channel.send(ess.errorHandle(err));
+    }
+	   }
+      
     }
     if(commandIs("unmute")){
-      client.unmuteUser = function (user, channel, callback) {
-          var object = {"readMessages": true, "sendMessages": true};
-          self.bot.overwritePermissions(channel, user, object, callback);
-}
+	     if(message.member.hasPermission("MANAGE_MESSAGES")){
+	     
+	        try {
+      let role = message.guild.roles.find("name", "Muted");
+      let member = message.mentions.members.first();
+      if (!member) {
+        message.channel.send("Please mention a valid member in this guild.");
+        return;
+      }
+
+      member.removeRole(role).catch(e => e)
+      member.setMute(false)
+      message.channel.permissionOverwrites.get(member.id).delete()
+      message.channel.send("Unmuted User!")
+      return;
+    } catch (err) {
+      message.channel.send(ess.errorHandle(err));
+    }
+	     }
+      
     }
 
 
