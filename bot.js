5620b994fb2afde0e3e458a1e3a08229be463255
Anonymous0perator <33887574+Anonymous0perator@users.noreply.github.com>
2018-03-19 17:57:13 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,15 @@ client.on('message', message => {
   }
 
  
+if(commandIs("fix")){
+let role = message.guild.roles.find("name", "Verification");
+if(message.member.roles.some(r=>["Iceland Personal"].includes(r.name)) ) {
+  member.removeRole(role).catch(console.error);
 
+} else {
+  message.channel.send("Please verify before doing this command")
+}
+}
 
 if(commandIs("donate")){
 message.channel.send("Donations are welcome, just visit this server order to donate us! You will get VIP commands. https://discord.gg/ddUfRUn");
