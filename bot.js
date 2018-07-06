9920693e958bf5b3139aacd49da9e6fc36eba1d8
Anonymous0perator <33887574+Anonymous0perator@users.noreply.github.com>
2018-07-07 03:03:05 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,27 +1,28 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
+const config = require('./config.json')
+const fs = require("fs")
+const groupId = 4205353;
+const roblox = require('roblox-js');
 
 client.on('ready', () => {
-  console.log('Bot is up and running!');
+  console.log('System is ready');
   client.user.setPresence({
     game: {
       name :(``),
       type: 0
     }
   });
-
+  client.guilds.array().forEach(function(element) {
+  },);
 });
 
-const prefix = ':'
+const prefix = config.prefix
+
 client.on('message', message => {
-  // Return if it is a bot
+
   if (message.author.bot) return;
   // Return if it is a DM
-  if (!message.guild) {
-    message.channel.send("You cannot do any of the bot's commands in DM")
-    return;
-  }
 
   // Saving memory, if there is no prefix it quits.
   if (!message.content.slice(1) === prefix) return;
@@ -34,81 +35,87 @@ client.on('message', message => {
     }
   }
 
-  // Whois Command
-  if (commandIs("userinfo")) {
-   try {
-      if (message.mentions.members.first()) {
-        let member = message.mentions.members.first().user
-        let guildMember = message.mentions.members.first()
-        const embed = new Discord.RichEmbed()
-          .setDescription("Description and information about " + member.tag)
-          .setAuthor(member.username, member.displayAvatarURL)
-          .setColor(0x70b080)
-          .setThumbnail(member.displayAvatarURL)
-          .setTimestamp(new Date())
-          .setFooter("Alpha", client.user.avatarURL)
-          .addField("ID", member.id)
-          .addField("Discriminator", member.discriminator)
-          .addField("Status", member.presence.status)
+if(commandIs("userinfo")){
+  try {
+     if (message.mentions.members.first()) {
+       let member = message.mentions.members.first().user
+       let guildMember = message.mentions.members.first()
+       const embed = new Discord.RichEmbed()
+         .setDescription("Description and information about " + member.tag)
+         .setAuthor(member.username, member.displayAvatarURL)
+         .setColor(3447003)
+         .setThumbnail(member.displayAvatarURL)
+         .setTimestamp(new Date())
+         .setFooter("Kevlar Beta", client.user.avatarURL)
+         .addField("ID", member.id)
+         .addField("Discriminator", member.discriminator)
+         .addField("Status", member.presence.status)
 
-          .addField("Nickname", guildMember.nickname)
-          .addField("Moderator", guildMember.hasPermission("BAN_MEMBERS"))
-          .addField("Joined at", guildMember.joinedAt)
-          .addField("Role(s)", guildMember.roles.array().join(", "))
-        message.channel.send(embed)
-      } else {
-        let member = message.author
-        let guildMember = message.guild.member(member)
-        const embed = new Discord.RichEmbed()
-          .setDescription("Description and information about " + member.tag)
-          .setAuthor(member.username, member.displayAvatarURL)
-          .setColor(0x70b080)
-          .setThumbnail(member.displayAvatarURL)
-          .setTimestamp(new Date())
-          .setFooter("Alpha", client.user.avatarURL)
-          .addField("ID", member.id)
-          .addField("Discriminator", member.discriminator)
-          .addField("Status", member.presence.status)
+         .addField("Nickname", guildMember.nickname)
+         .addField("Moderator", guildMember.hasPermission("BAN_MEMBERS"))
+         .addField("Joined at", guildMember.joinedAt)
+         .addField("Role(s)", guildMember.roles.array().join(", "))
+       message.channel.send(embed)
+     } else {
+       let member = message.author
+       let guildMember = message.guild.member(member)
+       const embed = new Discord.RichEmbed()
+         .setDescription("Description and information about " + member.tag)
+         .setAuthor(member.username, member.displayAvatarURL)
+         .setColor(3447003)
+         .setThumbnail(member.displayAvatarURL)
+         .setTimestamp(new Date())
+         .setFooter("Kevlar Beta", client.user.avatarURL)
+         .addField("ID", member.id)
+         .addField("Discriminator", member.discriminator)
+         .addField("Status", member.presence.status)
 
-          .addField("Nickname", guildMember.nickname)
-          .addField("Moderator", guildMember.hasPermission("BAN_MEMBERS"))
-          .addField("Joined at", guildMember.joinedAt)
-          .addField("Role(s)", guildMember.roles.array().join(", "))
-        message.channel.send(embed)
+         .addField("Nickname", guildMember.nickname)
+         .addField("Moderator", guildMember.hasPermission("BAN_MEMBERS"))
+         .addField("Joined at", guildMember.joinedAt)
+         .addField("Role(s)", guildMember.roles.array().join(", "))
+       message.channel.send(embed)
+     }
+     return;
+   } catch (err) {
+     message.channel.send(ess.errorHandle(err));
+   }
+}
+
+if(commandIs("buy")){
+  message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "Kevlar Beta",
+    url: "",
+    description: "Kevlar Beta helps your discord server, and connects discord and roblox.",
+    fields: [
+      {
+        name: "Plan",
+        value: "You can find monthly plan [here](https://docs.google.com/document/d/1ifi0dCPOgsRpdPx0K_LzqjrTmb8vB1KxUyw30SvMIYg/edit?usp=sharing)"
       }
-      return;
-    } catch (err) {
-      message.channel.send(ess.errorHandle(err));
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "Kevlar Beta"
     }
   }
-
-
-  if (commandIs("help")) {
-    const embed = new Discord.RichEmbed()
-      .setTitle("Commands List for Alpha")
-      .setDescription(`All the commands provided for the release version of Alpha. Default prefix is ${prefix}`)
-      .setColor(0x70b080)
-      .addField("help", "This help panel")
-    	.addField("membercount","Counts member")
-      .addField("serverinfo", "Information about the server")
-      .addField("botping", "Shows ping (message round trip) of the bot")
-      .addField("userinfo", "Information about user in the server")
-      .addField("purge", "Delete a bulk load of messages (100 max)")
-      .addField("kick", "Kicks a member from the server")
-      .addField("warn", "It will warn the people who you tagged")
- 
-      .setFooter("Alpha", client.user.avatarURL)
-      .setThumbnail(client.user.avatarURL)
-
-    message.channel.send(embed);
-  }
+});
+}
+if(commandIs('update', message)){
+  message.channel.send("All of the commands are disabled.")
+}
 if(commandIs("membercount")){
 	try {
       let guild = message.guild
 
       const embed = new Discord.RichEmbed()
         .setDescription("Membercount")
-        .setColor(0x70b080)
+        .setColor(3447003)
         .setThumbnail(guild.iconURL)
         .setTimestamp(new Date())
         .addField("Name", guild.name, true)
@@ -123,66 +130,7 @@ if(commandIs("membercount")){
       message.channel.send(ess.errorHandle(err));
     }
 }
-
-	
-    if(commandIs("mute")) {
-	   if(message.member.hasPermission("MANAGE_MESSAGES")){
-	   try {
-      let role = message.guild.roles.find("name", "Muted");
-      let member = message.mentions.members.first();
-      let bot = message.guild.member(client.user);
-
-      if (!member) {
-        message.channel.send("Please mention a valid member in this guild.");
-        return;
-      }
-      if (!role) {
-        message.guild.createRole({
-          name: 'Muted',
-          color: 'BLACK',
-        }).catch(err => message.channel.send(ess.errorHandle(err)))
-        message.channel.send("There was no Muted role so I created it. Try the mute command again.")
-        return;
-      }
-
-      member.setMute(true)
-      message.channel.overwritePermissions(member, {
-        "SEND_MESSAGES": false
-      })
-      member.addRole(role)
-      message.channel.send("Muted!")
-    } catch (err) {
-      message.channel.send(ess.errorHandle(err));
-    }
-	   }
-      
-    }
-    if(commandIs("unmute")){
-	     if(message.member.hasPermission("MANAGE_MESSAGES")){
-	     
-	        try {
-      let role = message.guild.roles.find("name", "Muted");
-      let member = message.mentions.members.first();
-      if (!member) {
-        message.channel.send("Please mention a valid member in this guild.");
-        return;
-      }
-
-      member.removeRole(role).catch(e => e)
-      member.setMute(false)
-      message.channel.permissionOverwrites.get(member.id).delete()
-      message.channel.send("Unmuted User!")
-      return;
-    } catch (err) {
-      message.channel.send(ess.errorHandle(err));
-    }
-	     }
-      
-    }
-
-
-
-  if (commandIs("purge")) {
+if (commandIs("purge")) {
     if (message.member.hasPermission("MANAGE_MESSAGES")) {
      try {
       if (!Number(message.content.split(" ")[1])) {
@@ -206,43 +154,12 @@ if(commandIs("membercount")){
     }
     }
   }
-  if(commandIs("warn")){
-
-    try {
-      let member = message.mentions.members.first();
-      if (!member) {
-        message.channel.send("Please mention a valid member in this guild.");
-        return;
-      }
-      if (!member.kickable) {
-        message.channel.send("I cannot warn this user. Please check permissions.");
-        return;
-      }
-
-      let reason = message.content.split(" ").slice(2).join(" ")
-      if (!reason) {
-        message.channel.send("Please indicate a reason for the warn!");
-        return;
-      }
-
-      member.send(`You have been warned for ${reason}`)
-      message.channel.send(`${member.user.tag} has been warned by ${message.author.tag} of the reason that ${reason}`);
-      return;
-
-
-    } catch (err) {
-      message.channel.send(ess.errorHandle(err));
-    }
-  }
-
-
 
 })
 
+  // Return if it is a bot
 
 
-
-	  
 
 
 
